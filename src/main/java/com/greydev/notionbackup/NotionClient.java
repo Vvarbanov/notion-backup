0d9636afd5bded6b0e5d241ccd8619067819fc7e
Vvarbanov <33352661+Vvarbanov@users.noreply.github.com>
2025-04-27 18:30:58 +0300

    fix(NotionClient): Adapt to using file token for new format

--- src/main/java/com/greydev/notionbackup/NotionClient.java
+++ src/main/java/com/greydev/notionbackup/NotionClient.java
@@ -2,13 +2,13 @@ package com.greydev.notionbackup;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.greydev.notionbackup.model.Result;
 import io.github.cdimascio.dotenv.Dotenv;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -23,16 +23,19 @@ import java.util.Optional;
 public class NotionClient {
 
     private static final int FETCH_DOWNLOAD_URL_RETRY_SECONDS = 5;
+    private static final int TRIGGER_EXPORT_TASK_RETRY_SECONDS = 5;
 
-	private static final String ENQUEUE_ENDPOINT = "https://www.notion.so/api/v3/enqueueTask";
+    private static final String ENQUEUE_ENDPOINT = "https://www.notion.so/api/v3/enqueueTask";
     private static final String NOTIFICATION_ENDPOINT = "https://www.notion.so/api/v3/getNotificationLogV2";
     private static final String TOKEN_V2 = "token_v2";
+    private static final String FILE_TOKEN = "file_token";
     private static final String EXPORT_FILE_NAME = "notion-export";
     private static final String EXPORT_FILE_EXTENSION = ".zip";
 
     private static final String KEY_DOWNLOADS_DIRECTORY_PATH = "DOWNLOADS_DIRECTORY_PATH";
     private static final String KEY_NOTION_SPACE_ID = "NOTION_SPACE_ID";
     private static final String KEY_NOTION_TOKEN_V2 = "NOTION_TOKEN_V2";
+    private static final String KEY_NOTION_FILE_TOKEN = "NOTION_FILE_TOKEN";
     private static final String KEY_NOTION_EXPORT_TYPE = "NOTION_EXPORT_TYPE";
     private static final String KEY_NOTION_FLATTEN_EXPORT_FILETREE = "NOTION_FLATTEN_EXPORT_FILETREE";
     private static final String KEY_NOTION_EXPORT_COMMENTS = "NOTION_EXPORT_COMMENTS";
@@ -43,6 +46,7 @@ public class NotionClient {
 
     private final String notionSpaceId;
     private final String notionTokenV2;
+    private final String notionFileToken;
     private final String exportType;
     private final boolean flattenExportFileTree;
     private final boolean exportComments;
@@ -57,6 +61,7 @@ public class NotionClient {
         // both environment variables and variables defined in the .env file can be accessed this way
         notionSpaceId = dotenv.get(KEY_NOTION_SPACE_ID);
         notionTokenV2 = dotenv.get(KEY_NOTION_TOKEN_V2);
+        notionFileToken = dotenv.get(KEY_NOTION_FILE_TOKEN);
         downloadsDirectoryPath = dotenv.get(KEY_DOWNLOADS_DIRECTORY_PATH);
 
         if (StringUtils.isBlank(downloadsDirectoryPath)) {
@@ -95,8 +100,8 @@ public class NotionClient {
     public Optional<File> export() {
         try {
             long exportTriggerTimestamp = System.currentTimeMillis();
-			// TODO - taskId is not really needed anymore
-			Optional<String> taskId = triggerExportTask();
+            // TODO - taskId is not really needed anymore
+            Optional<String> taskId = triggerExportTask();
 
             if (taskId.isEmpty()) {
                 log.info("taskId could not be extracted");
@@ -130,6 +135,7 @@ public class NotionClient {
             }
 
             log.info("Download finished: {}", downloadedFile.get().getName());
+            log.info("File size: {} bytes", downloadedFile.get().length());
             return downloadedFile;
         } catch (IOException | InterruptedException e) {
             log.warn("Exception during export", e);
@@ -141,6 +147,7 @@ public class NotionClient {
     private Optional<File> downloadToFile(String url, Path downloadPath) {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
+                .header("Cookie", FILE_TOKEN + "=" + notionFileToken)
                 .GET()
                 .build();
 
@@ -156,16 +163,25 @@ public class NotionClient {
 
 
     private Optional<String> triggerExportTask() throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(ENQUEUE_ENDPOINT))
-                .header("Cookie", TOKEN_V2 + "=" + notionTokenV2)
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(getTaskJson()))
-                .build();
+        for (int i = 0; i < 500; i++, sleep(TRIGGER_EXPORT_TASK_RETRY_SECONDS)) {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(ENQUEUE_ENDPOINT))
+                    .header("Cookie", TOKEN_V2 + "=" + notionTokenV2)
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(getTaskJson()))
+                    .build();
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        JsonNode responseJsonNode = objectMapper.readTree(response.body());
+            var statusCode = response.statusCode();
+            if (statusCode == HttpURLConnection.HTTP_BAD_GATEWAY
+                    || statusCode == HttpURLConnection.HTTP_GATEWAY_TIMEOUT) {
+                log.error("Server timeout (HTTP {}). Trying again in {} seconds...",
+                        statusCode, TRIGGER_EXPORT_TASK_RETRY_SECONDS);
+                continue;
+            }
+
+            JsonNode responseJsonNode = objectMapper.readTree(response.body());
 
 		/*	This will be the response if the given token is not valid anymore (for example if a logout occurred)
 			{
@@ -175,79 +191,78 @@ public class NotionClient {
 				"clientData":{"type":"login_try_again"}
 			}
 		 */
-        if (responseJsonNode.get("taskId") == null) {
-            JsonNode errorName = responseJsonNode.get("name");
-            log.error("Error name: {}, error message: {}", errorName, responseJsonNode.get("message"));
-            if (StringUtils.equalsIgnoreCase(errorName.toString(), "UnauthorizedError")) {
-                log.error("UnauthorizedError: seems like your token is not valid anymore. Try to log in to Notion again and replace you old token.");
+            if (responseJsonNode.get("taskId") == null) {
+                JsonNode errorName = responseJsonNode.get("name");
+                log.error("Error name: {}, error message: {}", errorName, responseJsonNode.get("message"));
+                if (StringUtils.equalsIgnoreCase(errorName.toString(), "UnauthorizedError")) {
+                    log.error("UnauthorizedError: seems like your token is not valid anymore. Try to log in to Notion again and replace you old token.");
+                }
+                return Optional.empty();
             }
-            return Optional.empty();
+
+            return Optional.of(responseJsonNode.get("taskId").asText());
         }
-
-        return Optional.of(responseJsonNode.get("taskId").asText());
+        return Optional.empty();
     }
-
 
     private Optional<String> fetchDownloadUrl(long exportTriggerTimestamp) throws IOException, InterruptedException {
         try {
-			for (int i = 0; i < 500; i++) {
-				sleep(FETCH_DOWNLOAD_URL_RETRY_SECONDS);
+            for (int i = 0; i < 500; i++) {
+                sleep(FETCH_DOWNLOAD_URL_RETRY_SECONDS);
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(NOTIFICATION_ENDPOINT))
-                .header("Cookie", TOKEN_V2 + "=" + notionTokenV2)
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(getNotificationJson()))
-                .build();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(NOTIFICATION_ENDPOINT))
+                        .header("Cookie", TOKEN_V2 + "=" + notionTokenV2)
+                        .header("Content-Type", "application/json")
+                        .POST(HttpRequest.BodyPublishers.ofString(getNotificationJson()))
+                        .build();
 
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-				JsonNode rootNode = objectMapper.readTree(response.body());
+                var statusCode = response.statusCode();
+                if (statusCode == HttpURLConnection.HTTP_BAD_GATEWAY
+                        || statusCode == HttpURLConnection.HTTP_GATEWAY_TIMEOUT) {
+                    log.error("Server timeout (HTTP {}). Trying again in {} seconds...",
+                            statusCode, FETCH_DOWNLOAD_URL_RETRY_SECONDS);
+                    continue;
+                }
 
-            JsonNode node = rootNode.path("recordMap");
-				node = node.path("activity");
-				node = node.fields().next().getValue();
-				node = node.path("value");
+                JsonNode rootNode = objectMapper.readTree(response.body());
 
-            long notificationStartTimestamp = Long.parseLong(node.path("start_time").asText());
+                JsonNode node = rootNode.path("recordMap");
+                node = node.path("activity");
+                node = node.fields().next().getValue();
+                node = node.path("value");
 
-				// we want the notification newer than the export trigger timestamp
-				// since the Notion response also contains older export trigger notifications
-				if (notificationStartTimestamp < exportTriggerTimestamp) {
-                log.info("The newest export trigger notification is still not in the Notion response. " +
-							"Trying again in {} seconds...", FETCH_DOWNLOAD_URL_RETRY_SECONDS);
-                continue;
-            }log.info("Found a new export trigger notification in the Notion response. " +
-						"Attempting to extract the download URL. " +
-						"Timestamp of when the export was triggered: {}. " +
-						"Timestamp of the notification: {}", exportTriggerTimestamp, notificationStartTimestamp);
+                long notificationStartTimestamp = Long.parseLong(node.path("start_time").asText());
 
-			Result result = results.getResults().get(0);
+                // we want the notification newer than the export trigger timestamp
+                // since the Notion response also contains older export trigger notifications
+                if (notificationStartTimestamp < exportTriggerTimestamp) {
+                    log.info("The newest export trigger notification is still not in the Notion response. " +
+                            "Trying again in {} seconds...", FETCH_DOWNLOAD_URL_RETRY_SECONDS);
+                    continue;
+                }
+                log.info("Found a new export trigger notification in the Notion response. " +
+                        "Attempting to extract the download URL. " +
+                        "Timestamp of when the export was triggered: {}. " +
+                        "Timestamp of the notification: {}", exportTriggerTimestamp, notificationStartTimestamp);
 
-			if (result.isFailure()) {
-				log.info("Notion API workspace export returned a 'failure' state. Reason: {}", result.getError());
-				return Optional.empty();
-			}
+                node = node.path("edits");
+                node = node.get(0);
+                JsonNode exportActivity = node.path("link");
 
-			if (result.getStatus() != null) {
-				log.info("Notion API workspace export 'state': '{}', Pages exported so far: {}", result.getState(), result.getStatus().getPagesExported());
-
-				if (StringUtils.isNotBlank(result.getStatus().getExportUrl())) {
-					log.info("Notion response now contains the export url. 'state': '{}', Pages exported so far: {}, Status.type: {}",
-							result.getState(), result.getStatus().getPagesExported(), result.getStatus().getType());
-				}
-			}
-
-			if (result.isSuccess()) {
-				log.info("Notion API workspace export 'state': '{}', Pages exported so far: {}", result.getState(), result.getStatus().getPagesExported());
-				return Optional.of(result.getStatus().getExportUrl());
-			}
-		sleep(6000);
-		}
-
-		log.info("Notion workspace export failed. After waiting 80 minutes, the export status from the Notion API response was still not 'success'");
-		return Optional.empty();
-	}
+                if (exportActivity.isMissingNode()) {
+                    log.info("The download URL is not yet present. Trying again in {} seconds...", FETCH_DOWNLOAD_URL_RETRY_SECONDS);
+                    continue;
+                }
+                return Optional.of(exportActivity.textValue());
+            }
+        } catch (Exception e) {
+            log.error("An exception occurred: ", e);
+        }
+        return Optional.empty();
+    }
 
     private String getTaskJson() {
         String taskJsonTemplate = "{" +
@@ -268,29 +283,27 @@ public class NotionClient {
         return String.format(taskJsonTemplate, notionSpaceId, exportComments, exportType.toLowerCase(), flattenExportFileTree);
     }
 
-	private String getNotificationJson() {
-		String notificationJsonTemplate = "{" +
-				"  \"spaceId\": \"%s\"," +
-				"  \"size\": 20," +
-				"  \"type\": \"unread_and_read\"," +
-				"  \"variant\": \"no_grouping\"" +
-				"}";
-		return String.format(notificationJsonTemplate, notionSpaceId);
-	}
+    private String getNotificationJson() {
+        String notificationJsonTemplate = "{" +
+                "  \"spaceId\": \"%s\"," +
+                "  \"size\": 20," +
+                "  \"type\": \"unread_and_read\"," +
+                "  \"variant\": \"no_grouping\"" +
+                "}";
+        return String.format(notificationJsonTemplate, notionSpaceId);
+    }
 
     private void exit(String message) {
         log.error(message);
         System.exit(1);
     }
 
-
     private void sleep(int seconds) {
         try {
-            Thread.sleep(seconds * 1000);
+            Thread.sleep(seconds * 1000L);
         } catch (InterruptedException e) {
             log.error("An exception occurred: ", e);
-			Thread.currentThread().interrupt();
+            Thread.currentThread().interrupt();
         }
     }
-
 }
